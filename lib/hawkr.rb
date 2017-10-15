9417d106b03ef00c5ce631b04c19d81918743fa9
Kado Nakamura <kadonakamura@protonmail.com>
2017-10-15 03:12:21 +0300

    threads

--- lib/hawkr.rb
+++ lib/hawkr.rb
@@ -8,6 +8,11 @@ require 'hawkr/version'
 require 'rom_boot'
 require_relative 'markets/livecoin'
 require_relative 'markets/gdax'
+require 'raven/base'
+
+Raven.configure do |config|
+  config.dsn = 'https://d9f6fd3569114796b4d606aee5cf3521:5619d906e1ea4aa2b987cef7c4ce7376@s.crypto.deals//4'
+end
 
 module Hawkr
 end
@@ -18,6 +23,14 @@ end
 
 repo = RomBoot.new.tickers_repo
 
-# Markets::Livecoin.new(repo: repo).start
-Markets::Gdax.new(repo: repo).start
+Raven.capture do
+  threads = []
+  [Markets::Livecoin, Markets::Gdax].each do |market|
+    threads << Thread.new do
+      market.new(repo: repo).start
+    end
+  end
+
+  threads.each(&:join)
+end
 puts 'done'
